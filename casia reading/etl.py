740288b63a382e7966d50de02d389a7aa5764675
thefxperson <thefxperson@gmail.com>
2019-01-03 23:04:56 -0800

    ETL database reading now works

--- casia reading/etl.py
+++ casia reading/etl.py
@@ -1,18 +1,40 @@
 import struct
-import tensorflow as tf
-from PIL import Image
+from PIL import Image, ImageEnhance
+ 
+filename = 'ETL9G/ETL9G_01'
+id_record = 1
+sz_record = 8199
+with open(filename, 'rb') as f:
+    f.seek(id_record * sz_record)
+    s = f.read(sz_record)
+    #unpack binary data from file into data array
+    #data is stored as followed:
+    #0 - Int - Serial Sheet Number
+    #1 - Int - JIS Character Code (JIS X 0208)
+    #2 - Bytes - JIS Typical Reading (string)
+    #3 - Int - Serial Data Number
+    #4-9 - Metadata. Likely all 0s.
+    #10 - Date of Collection (19YYMM)
+    #11 - Date of Scan (19YYMM)
+    #12 - X-Coordinate of Sample on Sheet
+    #13 - Y-Coordinate of Sample on Sheet
+    #14 - Bytes - Image (4bit 128x127 image)
+    #For more detailed information see ETL-9G: http://etlcdb.db.aist.go.jp/?page_id=1711 
+    data = struct.unpack('>2H8sI4B4H2B34x8128s7x', s)
+    print(data[0:14], hex(data[1]))
 
-num_records = 12144
-record_size = 8199
+    #convert from bytes to a properly encoded string
+    #it is unclear if character is encoded using the 1978 or 1983 JIS X 0208 standard. 83 is used here.
+    #b"\033$B" is the ISO2022 escape code from JIS X 0208-1983. b"\033$@" is for JIX X 0208-1978
+    #https://en.wikipedia.org/wiki/ISO/IEC_2022#ISO.2FIEC_2022_character_sets
+    print((b"\033$B" + data[1].to_bytes(2, byteorder="big")).decode("iso2022_jp"))
 
-characters = []
-images = []
+    #convert bytes to image
+    iF = Image.frombytes('F', (128, 127), data[14], 'bit', 4)
+    iP = iF.convert('P')
+    enhancer = ImageEnhance.Brightness(iP)
+    iE = enhancer.enhance(16)
 
-for file_num in range(50):
-    filename = "ETL9G/ETL9G_" + str(file_num+1).zfill(2)
-    with open(filename, "r") as file:
-        for record in range(num_records):
-            file.seek(record*record_size)
-            data = struct.unpack(">2H8sI4B4H2B34x8128s7x", file.read(record_size).encode("utf_8"))      #see ETL-9B specification
-            characters.append((b"\033$B" + data[1]).decode("iso2022_jp").encode("utf-8"))    #0b\033$B is an escape character which allows us to use JIS x 0208 (83) with ISO2022. Store as Unicode
-            images.append(Image.frombytes("F", (128, 127), data[14], "bit", 4).convert("P"))     #read image as floating, then convert to ints. 4 bits per pixel
+    #create the file name and save the image
+    fn = 'ETL9G_{:d}_{:s}.png'.format((data[0]-1)%20+1, hex(int(data[1]))[-4:])
+    iE.save(fn, 'PNG')
